976d16ee7e92de69cf875db42300999b8b7acbf2
Branden Kozel <branden.kozel@sinclair.edu>
2024-04-25 13:56:26 -0400

    Added the main and template half.

---
+++ S2 Week 15 - Part B/Source.cpp
@@ -0,0 +1,23 @@
+/*\
+Name: Branden Kozel
+Date: 4/25/24
+Class: 1202 201
+\*/
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+T half(T value) 
+{
+    return value / 2;
+}
+
+int main()
+{
+
+
+    system("pause");
+    return 0;
+}
